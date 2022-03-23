bddc58cf6a1e6116fce2c3d95a3d0162b142807f
Ahmad <muahmad710@gmail.com>
2022-03-24 04:01:45 +0500

    More on bits

---
+++ setUnsetBits.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+
+    int num = 4;
+    int x = num & 1<<2;
+    if (x == 0){
+        cout<<"NOT SET"<<endl;
+    }
+    else{
+        cout<<"SET"<<endl;
+    }
+    
+    // How to set a particular bit
+    num = num | 1<<1;
+    cout<<num<<endl;
+
+    // How to unset a bit
+    int val = 4;
+    val = val & ~(1<<2);
+    cout<<val<<endl;
+    return 0;
+}
