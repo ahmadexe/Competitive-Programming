66bcead4875f3a80ad9258f4715cdb5cbeaa76b0
Ahmad <muahmad710@gmail.com>
2022-03-24 03:33:16 +0500

    On bits

---
+++ bits.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+
+    int a = 100;
+    int b = a>>1;
+    cout<<b;
+
+    return 0;
+}
