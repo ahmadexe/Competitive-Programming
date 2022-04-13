280e09bd64538000ae492a97872fd0b05bfb515a
Ahmad <muahmad710@gmail.com>
2022-04-14 02:39:45 +0500

    Some new problems

--- PetrolPumpTours.cpp
+++ PetrolPumpTours.cpp
@@ -2,13 +2,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+int getThePump(vector<vector<int>> v)
+{
+    int i = 0;
+    while (i < v.size())
+    {
+        int distance = 0;
+        int j = i;
+        do{
+            
+        }while (j != i);
+    }
+}
 
 int main(int argc, char const *argv[])
 {
 
     vector<vector<int>> v = {{4,6},{6,5},{7,3},{4,5}};
-    int i = 0;
+    
     
     
 
