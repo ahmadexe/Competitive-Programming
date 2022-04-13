60d547d02b1bae68ca37d75ec12225699bacfe2d
Ahmad <muahmad710@gmail.com>
2022-04-14 02:46:15 +0500

    Some new problems

--- PetrolPumpTours.cpp
+++ PetrolPumpTours.cpp
@@ -11,27 +11,24 @@ int getThePump(vector<vector<int>> v)
         int distance = 0;
         int j = i;
         do{
-            int nextDistance = v[j][0] - v[j][1];
-            if (nextDistance >= 0)
-            {
-                distance += nextDistance;
-            }
-            else
-            {
-                foundThePump = false;
-                break;
-            }
-            j = (j + 1) % (v.size()-1);
+            
+
+            
         }while (j != i);
-        
+        if (foundThePump)
+        {
+            return i;
+        }
+        i++;
     }
+    return -1;
 }
 
 int main(int argc, char const *argv[])
 {
 
     vector<vector<int>> v = {{4,6},{6,5},{7,3},{4,5}};
-    
+    cout<<getThePump(v);   
     
     
 
