11c957bb6c035cecd66c7a211d427b460d7cfc14
Ahmad <muahmad710@gmail.com>
2022-07-27 05:43:02 +0500

    Cool name accepted

---
+++ checks.py
@@ -0,0 +1,1 @@
+print(type(ord('a')))
---
+++ coolname.py
@@ -0,0 +1,16 @@
+
+def solve(string):
+    power = 0
+    sorted_string = "".join(sorted(string))
+    for i in range(len(string)):
+        val = ord(sorted_string[i]) - 97
+        val += 1
+        power += (val * (i+1))
+    return power
+    
+
+t = int(input())
+while (t != 0):
+    string = input()
+    print(solve(string))
+    t -= 1
